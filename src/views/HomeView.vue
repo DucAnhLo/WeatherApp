04a7b0774242648b9863db37401be18807ae7542
ducanhlo <liamloducanh@gmail.com>
2023-08-19 18:42:01 +0100

    Done reusable model, going to build search function using MapBox Geocoding API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,20 @@
-<script setup>
-</script>
-
 <template>
-  <main>
+  <main class="container text-white">
+      <div class=" pt-4 mb-8 relative">
+        <input
+        v-model="searchQuery"
+        type="text" 
+        placeholder="Search for a city or state"
+        class="py-2 px-1 w-full bg-transparent border rounded-lg focus:border-weather-secondary focus:outline-none"
+        >
+      </div>
   </main>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const searchQuery = ref("")
+
+</script>
+

630f059ea2b341e8c5e81a389b043ef219802925
ducanhlo <liamloducanh@gmail.com>
2023-08-20 12:06:22 +0100

    redirect to route after click on search result done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,9 +12,11 @@
         v-if="mapboxSearchResults">
         <p v-if="searchError">Something went wrong, please try again.</p>
         <p v-if="!serviceError && mapboxSearchResults.length === 0">No results match your query, try a different term</p>
-          <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer"  @click="previewCity(searchResult)">
-            {{ searchResult.place_name }}
-          </li>
+          <template v-else>
+            <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer"  @click="previewCity(searchResult)">
+              {{ searchResult.place_name }}
+            </li>
+          </template>
         </ul>
       </div>
   </main>
@@ -23,12 +25,29 @@
 <script setup>
 import { ref } from 'vue';
 import axios from 'axios'
+import { useRouter } from 'vue-router';
+
 
 const mapboxAPIKey = "pk.eyJ1IjoiZHVjYW5obG8iLCJhIjoiY2xsaWIwZHc1MWFuYjNnbzM4aHdyb3prNCJ9.ig-0-rNY6TYdC_nJI7sJ9w"
 const searchQuery = ref("")
 const queryTimeOut = ref(null)
 const mapboxSearchResults = ref(null)
 const searchError = ref(null)
+const route = useRouter()
+
+const previewCity = (searchResult) => {
+  console.log(searchResult)
+  const [city, state] = searchResult.place_name.split(",")
+  route.push({
+    name: "cityView",
+    params: {state: state.replaceAll(" ", ""), city: city},
+    query: {
+      lat: searchResult.geometry.coordinates[1],
+      lg: searchResult.geometry.coordinates[0],
+      preview: true
+    }
+  })
+}
 
 const getSearchResults = () => {
   clearTimeout(queryTimeOut.value);
